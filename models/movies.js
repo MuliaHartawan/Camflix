c58d0c25cebad2c82370857393aa70aa1f178fcf
MuliaHartawan <mulia.hartawan011@gmail.com>
2022-11-11 22:59:30 +0700

    CRUD table Users

--- models/movies.js
+++ models/movies.js
@@ -1,3 +1,7 @@
+const {
+    Model
+} = require('sequelize');
+
 module.exports = (sequelize, DataTypes) => {
     const Movie = sequelize.define('Movie', {
         id : {
@@ -37,8 +41,13 @@ module.exports = (sequelize, DataTypes) => {
         }
     }, {
         tableName : 'movies',
-        timestamps : true
+        timestamps : true   
     });
+
+    // static associate(models) {
+    //     classroom.hasMany(models.student_schedule, { foreignKey: 'room_code' });
+        
+    // }
 
     return Movie;
 }
