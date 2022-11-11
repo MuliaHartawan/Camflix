0a4bb8c781775bfbfb3e4547bd93da2bddaec04c
MuliaHartawan <mulia.hartawan011@gmail.com>
2022-11-11 21:59:22 +0700

    create seeder users

---
+++ seeders/20221111145504-user-seeders.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const bcrypt = require('bcrypt');
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+     await queryInterface.bulkInsert('users', [
+      {
+        name: 'admin',
+        email : 'admin@gmail.com',
+        password : await bcrypt.hash('admin', 10),
+        created_at : new Date(),
+        updated_at : new Date(),
+      },
+    ], {});
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+     await queryInterface.bulkDelete('users', null, {});
+  }
+};
